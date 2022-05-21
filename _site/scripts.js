4a36b247d327c71ba6fefa7272e2f7c592231cee
minasweb <102960172+minasweb@users.noreply.github.com>
2022-05-21 09:47:50 +0200

    header, about and gallery start

---
+++ _site/scripts.js
@@ -0,0 +1,16 @@
+
+// A $( document ).ready() block.
+$(document).ready(function() {
+    $(".hamburger-button").click(function() {
+        $(this).toggleClass("active");
+        $(".mobile-menu").fadeToggle()
+    });
+
+
+    lightGallery(document.getElementById('lightgallery'), {
+        speed: 500,
+       download: false
+       });
+       
+    
+    
